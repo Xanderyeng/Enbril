9918c2c8f1e3f680002e2cd6a024cc3fc662d299
Xanderyeng <50344667+Xanderyeng@users.noreply.github.com>
2023-02-15 17:09:04 +0300

    Testing Mailchimp API

--- src/containers/contact/handler.jsx
+++ src/containers/contact/handler.jsx
@@ -1,8 +1,9 @@
 import axios from 'axios';
 const listId = "3d44be569f";
-const apikey = "a3c4c6c51fbb4bdfe3aaf5bf144dd869-us14";
+const apikey = "d04894d8268a8ac2fb163950bd01b863-us14";
+// const apikey = "a3c4c6c51fbb4bdfe3aaf5bf144dd869-us14";
 
-// NEW API KEY -- a3c4c6c51fbb4bdfe3aaf5bf144dd869-us14
+// NEW API KEY -- d04894d8268a8ac2fb163950bd01b863-us14
 
 //  THE PREVIOUS KEY HAS BEEN REVOKED -- c3ac1c46c3d863ea4284e368c41b6224-us14
 
@@ -20,7 +21,7 @@ const handleFormSubmit = (values, actions) => {
     axios.post( `https://us14.api.mailchimp.com/3.0/lists/${listId}/members`, payload, 
     {
       headers: {
-        Authorization: `${apikey}`
+        Authorization: ` Basic ${apikey}`
       }
     })
     .then(response => {

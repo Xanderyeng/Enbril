a53ee32ee59234cb446146279d77ab4d87d41c6f
Xanderyeng <50344667+Xanderyeng@users.noreply.github.com>
2023-03-10 19:16:05 +0300

    Main website background stars realignment

--- src/App.jsx
+++ src/App.jsx
@@ -19,9 +19,9 @@ const App = () => {
       <Star color='rgba(0, 133, 255, 0.2)' top='450vh' right='-10vw' scale='1.8' blur='90px'/>
       <Star color='rgba(255, 35, 214, 1)' top='365vh' right='-10vw' scale='1' blur='120px'/>
       <Star color='rgba(62, 93, 255, 1)' top='355vh' right='-90vw' scale='1.3' blur='95px'/>
-      <Star color='rgba(255, 35, 214, 0.5)' top='510vh' right='0vw' scale='0.8' blur='110px'/>
-      <Star color='rgba(62, 93, 255, 1)' top='510vh' right='-90vw' scale='1.6' blur='90px'/>
-      <Star color='rgba(255, 35, 214, 1)' top='525vh' right='-90vw' scale='0.5' blur='120px'/>
+      <Star color='rgba(255, 35, 214, 0.5)' top='710vh' right='0vw' scale='1.8' blur='190px'/>
+      <Star color='rgba(62, 93, 255, 1)' top='630vh' right='-90vw' scale='2.6' blur='140px'/>
+      <Star color='rgba(255, 35, 214, 1)' top='725vh' right='-10vw' scale='0.5' blur='230px'/>
       <nav>
         <Navbar />
         <Hero />
